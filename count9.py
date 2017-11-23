54f741e92a212e369152d5b127f8c81ac21dee81
dwiki nuridhuha <dwiki.nuridhuha@gmail.com>
2017-11-23 16:24:05 +0700

    Signed-off-by: dwiki nuridhuha <dwiki.nuridhuha@gmail.com> optimasi range dan linenya

--- count9.py
+++ count9.py
@@ -20,46 +20,46 @@ cnt_down = 0
 #cap = cv2.VideoCapture(0)
 cap = cv2.VideoCapture('sip.mp4')
 
-#Propiedades del video
+#Properti video
 ##cap.set(3,160) #Width
 ##cap.set(4,120) #Height
 
-#Imprime las propiedades de captura a consola
+#Cetak properti pengambilan ke konsol
 for i in range(19): #range asli 19
     print i, cap.get(i)
 
 w = cap.get(3) #nilai asli 3
 h = cap.get(4) #nilai asli 4
-frameArea = h*w
-areaTH = frameArea/250
+frameArea = 2*(h*w) #nilai asli h*w
+areaTH = frameArea/250 #konstanta 250
 print 'Area Threshold', areaTH
 
-#Lineas de entrada/salida
-line_up = int(2*(h/5))
-line_down   = int(3*(h/5))
+#Jalur masuk / keluar
+line_up = int(2*(h/6)) #nilai asli int(2*(h/5))
+line_down   = int(3*(h/5)) #nilai asli int(3*(h/5))
 
-up_limit =   int(1*(h/5))
-down_limit = int(4*(h/5))
+up_limit =   int(1*(h/7)) #nilai asli int(1*(h/5))
+down_limit = int(4*(h/5)) #nilai asli int(4*(h/5))
 
 print "Red line y:",str(line_down)
 print "Blue line y:", str(line_up)
 line_down_color = (255,0,0)
 line_up_color = (0,0,255)
-pt1 =  [0, line_down];
-pt2 =  [w, line_down];
+pt1 =  [0, line_down]
+pt2 =  [w, line_down]
 pts_L1 = np.array([pt1,pt2], np.int32)
 pts_L1 = pts_L1.reshape((-1,1,2))
-pt3 =  [0, line_up];
-pt4 =  [w, line_up];
+pt3 =  [0, line_up]
+pt4 =  [w, line_up]
 pts_L2 = np.array([pt3,pt4], np.int32)
 pts_L2 = pts_L2.reshape((-1,1,2))
 
-pt5 =  [0, up_limit];
-pt6 =  [w, up_limit];
+pt5 =  [0, up_limit]
+pt6 =  [w, up_limit]
 pts_L3 = np.array([pt5,pt6], np.int32)
 pts_L3 = pts_L3.reshape((-1,1,2))
-pt7 =  [0, down_limit];
-pt8 =  [w, down_limit];
+pt7 =  [0, down_limit]
+pt8 =  [w, down_limit]
 pts_L4 = np.array([pt7,pt8], np.int32)
 pts_L4 = pts_L4.reshape((-1,1,2))
 
